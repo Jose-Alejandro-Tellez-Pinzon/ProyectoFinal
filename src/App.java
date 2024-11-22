3cd233f312b8e49d3b4711bec2ee3037408922cc
Juliana Benavides Reales <jbenavides09@misena.edu.co>
2024-11-21 19:23:59 -0500

    Prueba

--- src/App.java
+++ src/App.java
@@ -3,12 +3,7 @@ import viajeinterplanetario.ViajeInterPlanetario;
 public class App {
     public static void main(String[] args) throws Exception {
         
-    ViajeInterPlanetario.startTravel();
-
-    System.out.println("lo que yo quiera");
-
-
-    
+    ViajeInterPlanetario.startTravel();    
 
     }
 }
